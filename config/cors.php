aea233a1daf28650d71715c1a15f06b7a782a968
MudasirTech786 <m.mudasirasfi@gmail.com>
2026-05-12 16:06:32 +0500

    database seeder changes added

--- config/cors.php
+++ config/cors.php
@@ -18,8 +18,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
         'https://nizaamo-frontend.vercel.app',
     ],
 
